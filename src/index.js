35fdd82e36d43dfcc259460882d254047975a0ee
nejkica <jevsevar@kookaburra.si>
2020-05-04 17:17:47 +0200

    jsx tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+
+const App = function() {
+
+	const btnTxt = { text: 'click me' };
+
+	return <div>
+		<label className="label" htmlFor="name">
+			Enter name:
+		</label>
+		<input type="text" id="name" />
+		<button style={{ backgroundColor: 'blue', color: 'white' }}>{btnTxt.text}</button>
+	</div>
+
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<App />,
+	document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
